012a89a97532254d3e46271e2c673a735824024f
Evgeny Demchenko <little_pea@list.ru>
2015-01-21 13:34:15 +0800

    Save images as JPG by default

--- cmsplugin_bootstrap_carousel/models_default.py
+++ cmsplugin_bootstrap_carousel/models_default.py
@@ -10,6 +10,8 @@ from PIL import Image
 from cStringIO import StringIO
 
 DEF_SIZE = (800, 600)
+DEF_EXTENSION = 'jpg'
+DEF_TYPE = 'image/jpeg'
         
 
 class Carousel(CMSPlugin):
@@ -39,19 +41,18 @@ class CarouselItem(models.Model):
             img = Image.open(self.image.file)
             if img.mode not in ('L', 'RGB'):
                 img = img.convert('RGB')
-            if hasattr(settings, "BOOTSTRAP_CAROUSEL_IMGSIZE"):
-                size = settings.BOOTSTRAP_CAROUSEL_IMGSIZE
-            else:
-                size = DEF_SIZE
+            size = getattr(settings, "BOOTSTRAP_CAROUSEL_IMGSIZE", DEF_SIZE)
+            extension = getattr(settings, "BOOTSTRAP_CAROUSEL_FILE_EXTENSION", DEF_EXTENSION)
+            content_type = getattr(settings, "BOOTSTRAP_CAROUSEL_FILE_CONTENT_TYPE", DEF_TYPE)
             img.thumbnail(size, Image.ANTIALIAS)
 
             temp_handle = StringIO()
-            img.save(temp_handle, 'png')
+            img.save(temp_handle, extension)
             temp_handle.seek(0)
 
             suf = SimpleUploadedFile(os.path.split(self.image.name)[-1],
-                                     temp_handle.read(), content_type='image/png')
-            fname = "%s.png" % os.path.splitext(self.image.name)[0]
+                                     temp_handle.read(), content_type=content_type)
+            fname = "%s.%s" % (os.path.splitext(self.image.name)[0], extension)
             self.image.save(fname, suf, save=False)
 
         super(CarouselItem, self).save()
