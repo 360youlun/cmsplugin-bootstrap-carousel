9788688c9610caec101b929ec9d2008a8f0ca4c4
Lucian Corduneanu <lucian.corduneanu@sensidev.com>
2016-01-05 10:43:48 +0200

    #15060 Use the same fields for CarouselItem also for filer version, but only change the image field.

--- cmsplugin_bootstrap_carousel/models_filer.py
+++ cmsplugin_bootstrap_carousel/models_filer.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext as _
 from filer.fields.image import FilerImageField
 from cms.models.pluginmodel import CMSPlugin
 
+from cmsplugin_bootstrap_carousel.models_default import CarouselItem
 
 DEF_SIZE = (800, 600)
         
@@ -22,8 +23,6 @@ class Carousel(CMSPlugin):
     def __unicode__(self):
         return self.domid
 
-class CarouselItem(models.Model):
-    carousel = models.ForeignKey(Carousel)
-    caption_title = models.CharField(max_length=100, blank=True, null=True)
-    caption_content = models.TextField(blank=True, null=True)
+
+class CarouselItem(CarouselItem):
     image = FilerImageField(blank=True, null=True)
