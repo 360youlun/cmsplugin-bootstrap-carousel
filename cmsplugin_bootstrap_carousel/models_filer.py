ab61b4bd188033a11db19c122b0a2a0ef0db7653
Lucian Corduneanu <lucian.corduneanu@sensidev.com>
2016-01-05 11:08:21 +0200

    #15060 Drop the filer support.

--- cmsplugin_bootstrap_carousel/models_filer.py
+++ cmsplugin_bootstrap_carousel/models_filer.py
@@ -6,23 +6,24 @@ from django.utils.translation import ugettext as _
 from filer.fields.image import FilerImageField
 from cms.models.pluginmodel import CMSPlugin
 
-from cmsplugin_bootstrap_carousel.models_default import CarouselItem
 
 DEF_SIZE = (800, 600)
-        
+
 class Carousel(CMSPlugin):
     domid = models.CharField(max_length=50, verbose_name=_('Name'))
     interval = models.IntegerField(default=5000)
-    
+
     def copy_relations(self, oldinstance):
         for item in oldinstance.carouselitem_set.all():
             item.pk = None
             item.carousel = self
             item.save()
-    
+
     def __unicode__(self):
         return self.domid
 
-
-class CarouselItem(CarouselItem):
+class CarouselItem(models.Model):
+    carousel = models.ForeignKey(Carousel)
+    caption_title = models.CharField(max_length=100, blank=True, null=True)
+    caption_content = models.TextField(blank=True, null=True)
     image = FilerImageField(blank=True, null=True)
